ab2bbd93b1e8f04ced68a730b6ee661fe581b074
Gustavo Morinaga <me@gustavomorinaga.dev>
2025-07-19 17:53:22 -0300

    feat: integrate AWS S3 for meal file uploads with presigned URL generation

--- apps/api/src/controllers/create-meal.controller.ts
+++ apps/api/src/controllers/create-meal.controller.ts
@@ -1,6 +1,10 @@
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import z from 'zod';
+import { s3Client } from '../clients/s3.client';
 import { db } from '../db/connection';
 import { schema } from '../db/schema';
+import { env } from '../env';
 import type { HttpResponse, ProtectedHttpRequest } from '../types/http.type';
 import { badRequest, ok } from '../utils/http.util';
 
@@ -19,21 +23,31 @@ export class CreateMealController {
 			return badRequest({ errors: error.issues });
 		}
 
+		const fileID = crypto.randomUUID();
+		const extension = data.fileType === 'audio/m4a' ? 'm4a' : 'jpeg';
+		const fileKey = `${fileID}.${extension}`;
+
+		const command = new PutObjectCommand({
+			Bucket: env.BUCKET_NAME,
+			Key: fileKey,
+		});
+		const presignedURL = await getSignedUrl(s3Client, command, {
+			expiresIn: 600,
+		});
+
 		const [meal] = await db
 			.insert(schema.meals)
 			.values({
 				userID,
-				inputFileKey: 'input_file_key',
+				inputFileKey: fileKey,
 				inputType: data.fileType === 'audio/m4a' ? 'audio' : 'picture',
 				status: 'uploading',
 				icon: '',
 				name: '',
 				foods: [],
 			})
-			.returning({
-				id: schema.users.id,
-			});
+			.returning({ id: schema.users.id });
 
-		return ok({ mealID: meal.id });
+		return ok({ mealID: meal.id, uploadURL: presignedURL });
 	}
 }
--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const envSchema = z.object({
 	DATABASE_URL: z.url().startsWith('postgresql://'),
 	JWT_SECRET: z.string().min(1),
+	BUCKET_NAME: z.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
